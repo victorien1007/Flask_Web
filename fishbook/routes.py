e22eb27f68163f121aeee4e8f182b546c6dd8d34
WANG Yi <victorien1007@users.noreply.github.com>
2019-01-21 19:43:35 +0100

    delete requests

--- fishbook/routes.py
+++ fishbook/routes.py
@@ -1,7 +1,6 @@
 import os.path
 import secrets
 import json
-from flask import requests
 from datetime import datetime
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, jsonify, Blueprint
