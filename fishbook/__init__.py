90dfdc77c4a1b9cb49e78edd5e53ded608fb13c9
WANG Yi <victorien1007@users.noreply.github.com>
2019-01-21 12:53:39 +0100

    Update __init__.py

--- fishbook/__init__.py
+++ fishbook/__init__.py
@@ -5,8 +5,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='58571003d3f79dab3ab900fe007d6cc957d0656fdb4c6bc4cab66a57f86ab614'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:test123456@localhost:5432/backend'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:test123456@34.76.51.130:5432/myfishbook'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:test123456@localhost:5432/backend'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
